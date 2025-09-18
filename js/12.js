0aa0ecbb59664e83ea034a781797e751df4fb221
ivan_omgru <loverussian62@gmail.com>
2025-09-19 00:55:02 +0300

    Update 12.js

--- js/12.js
+++ js/12.js
@@ -323,7 +323,7 @@ function initGallery({ galleryId, btnId, manualData, fetchApiFn, pageSize = 8 })
 }
 /* ------------------ MANUAL DATA ------------------ */
 const YT_MANUAL = [
-  {"@id":"https://youtube.ivan-omgru.ir/media/youtube/1.jpg","thumb":"https://youtube.ivan-omgru.ir/media/youtube/1.jpg","link":"https://www.youtube.com/@ivan.omgruss","pageLink":"https://youtube.ivan-omgru.ir/posts/youtube1.html","fa":"ویدیو معرفی سایت ivan_omgru","ru":"Видео: Введение в сайт ivan_omgru"}
+  {"@id":"https://youtube.ivan-omgru.ir/media/youtube/1.jpg","thumb":"https://youtube.ivan-omgru.ir/media/youtube/1.jpg","link":"https://www.youtube.com/@ivan.omgruss","fa":"ویدیو معرفی سایت ivan_omgru","ru":"Видео: Введение в сайт ivan_omgru"}
 ];
 const IG_MANUAL = [
   {"@id":"https://insta.ivan-omgru.ir/media/instagram/1.jpg","thumb":"https://insta.ivan-omgru.ir/media/instagram/1.jpg","link":"https://www.instagram.com/p/ChnSyX3pC-7/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==","pageLink":"https://insta.ivan-omgru.ir/posts/instagram1.html","fa":"پست 1","ru":"Пост 1"}
@@ -426,3 +426,4 @@ function showGalleryError(galleryId, message){
   g.innerHTML = '';
   g.appendChild(el);
 }
+
